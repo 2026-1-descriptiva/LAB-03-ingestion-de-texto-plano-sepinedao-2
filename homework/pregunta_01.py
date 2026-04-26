0e7015c3b9d2ed87cb1863f0058dc8f1b1f71590
sepinedao <sepinedao@unal.edu.co>
2026-04-26 10:52:53 -0500

    solucion_lab_3

--- homework/pregunta_01.py
+++ homework/pregunta_01.py
@@ -2,11 +2,87 @@
 Escriba el codigo que ejecute la accion solicitada en cada pregunta.
 """
 
-# pylint: disable=import-outside-toplevel
-
 
 def pregunta_01():
-    """
+    
+    import pandas as pd
+
+    with open("files/input/clusters_report.txt", "r", encoding="utf-8") as file:
+        lineas = file.readlines()
+
+    datos = []
+    fila_actual = None
+
+    for linea in lineas:
+        linea = linea.strip()
+
+        if linea == "":
+            continue
+
+        if linea.startswith("-"):
+            continue
+
+        partes = linea.split()
+
+        if len(partes) == 0:
+            continue
+
+        primera_parte = partes[0]
+
+        if primera_parte.isdigit():
+            if fila_actual is not None:
+                datos.append(fila_actual)
+
+            cluster = int(partes[0])
+            cantidad = int(partes[1])
+            porcentaje_texto = float(partes[2].replace(",", "."))
+            palabras_clave = ""
+
+            for palabra in partes[4:]:
+                palabras_clave = palabras_clave + " " + palabra
+
+            palabras_clave = palabras_clave.strip()
+
+            fila_actual = {
+                "cluster": cluster,
+                "cantidad_de_palabras_clave": cantidad,
+                "porcentaje_de_palabras_clave": porcentaje_texto,
+                "principales_palabras_clave": palabras_clave,
+            }
+
+        else:
+            if fila_actual is not None:
+                texto_adicional = " ".join(partes)
+                fila_actual["principales_palabras_clave"] = (
+                    fila_actual["principales_palabras_clave"] + " " + texto_adicional
+                )
+
+    if fila_actual is not None:
+        datos.append(fila_actual)
+
+    for fila in datos:
+        texto = fila["principales_palabras_clave"]
+
+        palabras = texto.split()
+        texto_limpio = " ".join(palabras)
+
+        partes = texto_limpio.split(",")
+        partes_limpias = []
+
+        for parte in partes:
+            partes_limpias.append(parte.strip())
+
+        texto_limpio = ", ".join(partes_limpias)
+
+        if texto_limpio.endswith("."):
+            texto_limpio = texto_limpio[:-1]
+
+        fila["principales_palabras_clave"] = texto_limpio
+
+    dataframe = pd.DataFrame(datos)
+
+    return dataframe
+"""
     Construya y retorne un dataframe de Pandas a partir del archivo
     'files/input/clusters_report.txt'. Los requierimientos son los siguientes:
 
@@ -18,3 +94,6 @@ def pregunta_01():
 
 
     """
+
+
+print(pregunta_01())
